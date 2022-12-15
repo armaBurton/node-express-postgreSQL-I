1f2d60a691d1e4331e5ffec34e05a87247863287
armaBurton <armanarma@armaburton.com>
2022-12-15 11:42:31 -0800

    $

---
+++ lib/utils/pool.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+
+//creating a pool object to get access to postgerSQL DB
+const pool: Pool = new Pool({
+  user: "postgres",
+  password: "password",
+  host: "localhost",
+  port: 7000,
+  database: "PERN REST API",
+});
+
+export default pool;
